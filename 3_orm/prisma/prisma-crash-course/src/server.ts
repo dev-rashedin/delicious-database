bbc920d491be4aaae778cb9aa40b4f539c1c1169
dev-rashedin <rashedinislam.06@gmail.com>
2026-02-01 23:11:32 +0600

    prisma initialized

--- 3_orm/prisma/prisma-crash-course/src/server.ts
+++ 3_orm/prisma/prisma-crash-course/src/server.ts
@@ -2,6 +2,9 @@ import app from './app';
 
 const port = process.env.PORT || 3000;
 
+console.log(port)
+
+
 app.listen(port, () => {
   console.log(`Server listening on port http://localhost:${port}`);
 });
