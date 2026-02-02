d608999288931d553d8f2f93820191451e962d25
dev-rashedin <rashedinislam.06@gmail.com>
2026-02-02 15:02:58 +0600

    prisma setup almost done

---
+++ 3_orm/prisma/prisma-crash-course/prisma/seed.ts
@@ -0,0 +1,9 @@
+// import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from './generated/prisma'
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
+async function seed() {
+
+}
