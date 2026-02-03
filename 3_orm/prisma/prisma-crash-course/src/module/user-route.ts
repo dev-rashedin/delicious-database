6db7adccaa77d3d7472d7a84eabbacae559fb295
dev-rashedin <rashedinislam.06@gmail.com>
2026-02-03 23:31:15 +0600

    user-route updated

--- 3_orm/prisma/prisma-crash-course/src/module/user-route.ts
+++ 3_orm/prisma/prisma-crash-course/src/module/user-route.ts
@@ -57,7 +57,45 @@ userRouter.get("/married-users", asyncHandler(async (_req: Request, res: Respons
  })
 }))
 
+// get user by id
+userRouter.get("/user/:id", asyncHandler(async (_req: Request, res: Response) => {
+ const user = await prisma.user.findUnique({
+   where: {
+     id: 4
+   }
+ })
 
+ res.status(StatusCodes.OK).json({
+   success: true,
+   message: "User fetched successfully",
+   data: user
+ })
+}))
+
+// update user by id
+userRouter.put("/user/:id", asyncHandler(async (req: Request, res: Response) => {
+
+  const id = req.params.id
+
+ const user = await prisma.user.update({
+   where: {
+     id: Number(id)
+   },
+   data: {
+     name: "Luna Lovegood",
+     age: 20,
+     isMarried: true,
+     nationality: "Irish",
+     email: "luna@hogwarts"
+   }
+ })
+
+ res.status(StatusCodes.OK).json({
+   success: true,
+   message: "User updated successfully",
+   data: user
+ })
+}))
 
 
 export default userRouter
